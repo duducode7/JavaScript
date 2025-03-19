78b43a2ee536e94fb8ba9eb6f5de774b752c83bc
Eduardo <eduardo_oliveira37@estudante.sesisenai.org.br>
2025-03-19 10:02:40 -0300

    UPDATE - questão 2

--- script.js
+++ script.js
@@ -16,5 +16,16 @@ function calcular (n1,n2){
     const soma = n1 + n2;
     const subtracao = n1 - n2;
     const divisao = n2 !== 0 ? n1/n2: 'Não pode dividir por zero';
-    
+    multiplicacao = n1 * n2;
+
+    console.log(soma);
+    console.log(subtracao);
+    console.log(divisao);
+    console.log(multiplicacao);
+
 }
+
+let n1 = parseFloat(prompt("Digite seu primeiro numero"))
+let n2 = parseFloat(prompt("Digite seu segundo numero"))
+
+calcular(n1,n2)

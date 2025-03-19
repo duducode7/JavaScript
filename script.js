dd4c5f999303d9682daebafbda723b644698fef3
Eduardo <eduardo_oliveira37@estudante.sesisenai.org.br>
2025-03-19 09:35:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function verificarPar(numero){
+    if(numero % 2 === 0) {
+        // === é estritamente igual, é igual em tipo e conteudo
+        console.log('par')
+    }else{
+        console.log('impar')
+    }
+}
+
+verificarPar(4);
+verificarPar(7);
+
+
